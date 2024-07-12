95127c708614533d7f0754bc34c538e6f576a8c6
Ruzibek <robidron0199@gmail.com>
2024-07-12 12:13:09 +0500

    Added Swagger and Redoc

--- E_Commerce/urls.py
+++ E_Commerce/urls.py
@@ -16,7 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E Commerce DRF",
+        default_version='v1',
+        description="This is test for leaning django rest api!",
+        terms_of_service="https://www.coming_soon.com/",
+        contact=openapi.Contact(email="ruzibek.work@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
+
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
 ]
